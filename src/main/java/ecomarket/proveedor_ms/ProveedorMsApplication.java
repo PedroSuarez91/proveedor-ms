8d98ad88ccc9b9f95e568a17bbf69cbe91281559
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-06-16 10:19:50 -0400

    creacion microservicio proveedor

---
+++ src/main/java/ecomarket/proveedor_ms/ProveedorMsApplication.java
@@ -0,0 +1,13 @@
+package ecomarket.proveedor_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveedorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecomarket/proveedor_ms/ProveedorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomarket.proveedor_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
